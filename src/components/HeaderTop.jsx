28d3153efba3becfc09fca3236418ea53f928c79
waltershaquil <81990995+waltershaquil@users.noreply.github.com>
2025-01-16 10:49:22 +0200

    fixed the padding

--- src/components/HeaderTop.jsx
+++ src/components/HeaderTop.jsx
@@ -2,7 +2,7 @@ import { BadgeDollarSign } from "lucide-react";
 
 function HeaderTop() {
   return (
-    <header className="bg-white">
+    <header className="bg-white px-10">
       <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 font-bold ">
         <a className=" text-teal-600 items-center inline-flex" href="#">
           <span className="sr-only">Home</span>
@@ -11,45 +11,31 @@ function HeaderTop() {
         </a>
 
         <div className="flex flex-1 items-center justify-end md:justify-between">
-          <nav aria-label="Global" className="hidden md:block">
-            <ul className="flex items-center gap-6 text-sm">
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75"
-                  href="#"
-                >
-                  {" "}
-                  About{" "}
-                </a>
-              </li>
-
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75"
-                  href="#"
-                >
-                  {" "}
-                  Projects{" "}
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <nav aria-label="Global" className="hidden md:block"></nav>
 
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
-              <a
-                className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
-                href="#"
-              >
-                Login
-              </a>
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-teal-600"
+                    href="#"
+                  >
+                    {" "}
+                    About{" "}
+                  </a>
+                </li>
 
-              <a
-                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-                href="#"
-              >
-                Register
-              </a>
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-teal-600"
+                    href="#"
+                  >
+                    {" "}
+                    Projects{" "}
+                  </a>
+                </li>
+              </ul>
             </div>
 
             <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
